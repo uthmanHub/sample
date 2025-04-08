b3d3b336948187b0cff43a7336dec787811b55fb
uthmanHub <uthmancod@gmail.com>
2025-04-08 04:03:42 +0100

    added

--- sample/settings.py
+++ sample/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1k&3y=j+klr34sd+#_(6p97$+qc8665t5#cpmyv*r2se7*y)br
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', '.now.sh']
 
 
 # Application definition
